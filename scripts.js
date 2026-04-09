fe53d8136a602590ce82b6985f67d3fae11e54ff
mayravitoriadias-hash <mayravitoriadias@gmail.com>
2026-04-09 17:42:14 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const item = document.querySelectorAll(".item");
+const dot = document.querySelectorAll(".dot");
+const numbersIndicators = document.querySelector(".numbers");
+const list = document.querySelector(".list");
+
+let active = 0;
+const total = item.length;
+let timer;
+
+function update(direction) {
+  document.querySelector(".item.active").classList.remove("active");
+  document.querySelector(".dot.active").classList.remove("active");
+
+  if (direction > 0) {
+    active = active + 1;
+    if (active === total) {
+      active = 0;
+    }
+  } else if (direction < 0) {
+    active = active - 1;
+    if (active < 0) {
+      active = total - 1;
+    }
+  }
+
+  item[active].classList.add("active");
+  dot[active].classList.add("active");
+
+numbersIndicators.textContent = String (active +1).padStart(2,'0')
+
+}
+
+clearInterval(timer);
+timer = setInterval(() => {
+  update(1);
+}, 5000);
+
+prevButton.addEventListener("click", () => {
+  update(-1);
+});
+
+nextButton.addEventListener("click", () => {
+  update(+1);
+});
